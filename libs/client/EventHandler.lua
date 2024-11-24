bcac657fd6f1405fd232c76a89fb1857d34086fd
Richy Z <hello@richy.lol>
2024-11-24 05:15:51 +0000

    Fix horrendous double requests when getting application ID
    Replaced self:getApplicationInformation().id with self._user._id to ensure that multiple HEAVILY UNNECESSARY requests are not made for each client method.

--- libs/client/EventHandler.lua
+++ libs/client/EventHandler.lua
@@ -2,27 +2,27 @@ local ApplicationCommand = require("containers/ApplicationCommand")
 local events = {}
 
 function events.APPLICATION_COMMAND_CREATE(d, client)
-	local cmd = ApplicationCommand(d, client)
+    local cmd = ApplicationCommand(d, client)
 
-	client:emit("applicationCommandCreate", cmd)
+    client:emit("applicationCommandCreate", cmd)
 end
 
 function events.APPLICATION_COMMAND_UPDATE(d, client)
-	local cmd = ApplicationCommand(d, client)
+    local cmd = ApplicationCommand(d, client)
 
-	p("APPLICATION_COMMAND_UPDATE", cmd)
+    p("APPLICATION_COMMAND_UPDATE", cmd)
 
-	client:emit("applicationCommandUpdate", cmd)
+    client:emit("applicationCommandUpdate", cmd)
 end
 
 function events.APPLICATION_COMMAND_DELETE(d, client)
-	local cmd = ApplicationCommand(d, client)
+    local cmd = ApplicationCommand(d, client)
 
-	client:emit("applicationCommandDelete", cmd)
+    client:emit("applicationCommandDelete", cmd)
 end
 
 function events.APPLICATION_COMMAND_PERMISSIONS_UPDATE(d, client)
-	client:emit("applicationCommandPermissionsUpdate", cmd)
+    client:emit("applicationCommandPermissionsUpdate", cmd)
 end
 
 return events
